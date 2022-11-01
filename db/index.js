c18f5b14be6d028e1b8de8eff053cae2a6e53f5f
Orlando <oredwardsjr@gmail.com>
2022-11-01 12:23:15 -0700

    Endpoints to DB works

--- db/index.js
+++ db/index.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.DATABASE, {
+const mongoose = require("mongoose");
+
+const conn = mongoose
+  .connect("mongodb+srv://dbUser:q40j1EvpwBc1kKE2@cluster0.altigvb.mongodb.net/?retryWrites=true&w=majority", {
     useUnifiedTopology: true,
     useNewURLParser: true,
+    dbName: "EmptyStack"
   })
   .then(() => console.log("Database connected"))
   .catch((e) => console.error("Database not connected:", e));
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express(); // server software
 const PORT = process.env.PORT;
 const bodyParser = require('body-parser')
+const db = require('./db/index');
+const { StudentsSchema } = require('./models')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -15,5 +17,32 @@ app.get('/test', (req, res) => {
         name: "This is a placeholder message for testing purposes."
     })
 });
+
+app.post('/test/db', async (req, res) => {
+    const payload = req.body;
+
+    if (!payload) {
+        return res.status(400).json({
+          success: false,
+          error: "Must include payload",
+        });
+    }
+
+    const student = new StudentsSchema(payload);
+
+    if (!student) {
+        return res.status(400).json({
+          success: false,
+          error: "Student not created",
+        });
+    }
+    
+    await student
+    .save()
+    .then(() => res.status(200).json({
+        success: true
+    }))
+    .catch(e => console.log(e))
+})
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));

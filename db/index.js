d55ab513eb333937c0fc4c2dd0c6642b1483c2c3
Dorian Majano <dorian.majano@gmail.com>
2022-11-01 08:00:00 -0700

    Connect to database

--- db/index.js
+++ db/index.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.DATABASE, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
+  .connect(
+    "mongodb+srv://dbUser:q40j1EvpwBc1kKE2@cluster0.altigvb.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewURLParser: true,
+    }
+  )
   .then(() => console.log("Database connected"))
   .catch((e) => console.error("Database not connected:", e));
-
-const db = mongoose.connection;
-
-module.exports = db;

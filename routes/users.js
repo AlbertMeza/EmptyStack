62ca5b9810e6912a76c5006df36ade97d4e6c682
Dorian Majano <dorian.majano@gmail.com>
2022-11-02 09:20:50 -0700

    Add Instructor to routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const connectEnsureLogin = require('connect-ensure-login'); //authorization
-const { loadRegistration, registerUser, logoutUser, getProfile, getUsers, attemptLogin } = require('../controllers/students-cntrl')
+const connectEnsureLogin = require("connect-ensure-login"); //authorization
+const {
+  loadRegistration,
+  registerUser,
+  logoutUser,
+  getProfile,
+  getUsers,
+  attemptLogin,
+} = require("../controllers/students-cntrl");
 
 // login endpoint is handled by html form
-router.post('/login', attemptLogin);
+router.post("/login", attemptLogin);
 
 // go to register page
-router.get('/register', loadRegistration);
+router.get("/register", loadRegistration);
 
 // // submit registration
-router.post('/register/attempt', registerUser);
+router.post("/register/attempt", registerUser);
 
 // // logout
-router.get('/logout', logoutUser);
+router.get("/logout", logoutUser);
 
 // // get user profile
-router.get('/profile', connectEnsureLogin.ensureLoggedIn(), getProfile);
+router.get("/profile", connectEnsureLogin.ensureLoggedIn(), getProfile);
 
 // // get all users
-router.get('/', getUsers);
+router.get("/", getUsers);
+
+const {
+  createInstructor,
+  getInstructors,
+  getInstructor,
+  updateInstructorMessage,
+} = require("./../controllers/instructor-ctrl");
+
+router.post("/instructor/create", createInstructor);
+router.get("/instructors", getInstructors);
+router.get("/instructor/:id", getInstructor);
+router.put("/instructor/:id/links/:id", updateInstructorMessage);
 
 module.exports = router;

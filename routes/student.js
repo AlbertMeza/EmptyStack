f2fca5513bc99ce4aed848facd1a667754ed2d29
Dorian Majano <dorian.majano@gmail.com>
2022-11-04 07:44:57 -0700

    Update student api

--- routes/student.js
+++ routes/student.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const { getStudents, getStudent } = require("./../controllers/student-ctrl");
+
+router.get("/students", getStudents);
+router.get("/student/:id", getStudent);
+
 module.exports = router;
